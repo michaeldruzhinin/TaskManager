a89eef2b8c7003ee693e83722e1282dc733591e6
michaeldruzhinin <michaeldruzhinin07@gmail.com>
2025-04-27 18:39:31 +0300

    working TaskManager class added + cmake fixes

--- core/taskmanager.cpp
+++ core/taskmanager.cpp
@@ -1,1 +1,47 @@
-// Реализация будет здесь
+#include "taskmanager.h"
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+void TaskManager::addTask(std::unique_ptr<Task> task) {
+    tasks.push_back(std::move(task));
+}
+
+void TaskManager::removeTask(std::size_t index) {
+    if (index < tasks.size()) {
+        tasks.erase(tasks.begin() + index);
+    }
+}
+
+void TaskManager::listTasks(bool showCompleted) const {
+    for (std::size_t i = 0; i < tasks.size(); ++i) {
+        const Task& task = *tasks[i];
+        if (showCompleted || !task.isCompleted()) {
+            std::cout << i << ") " << task.getTitle()
+                << " [" << (task.getPriority() == 2 ? "High" : task.getPriority() == 1 ? "Medium" : "Low") << "]"
+                << (task.isCompleted() ? " (Completed)" : "") << "\n";
+        }
+    }
+}
+
+void TaskManager::sortByPriority() {
+    std::sort(tasks.begin(), tasks.end(), [](const std::unique_ptr<Task>& a, const std::unique_ptr<Task>& b) {
+        return a->getPriority() > b->getPriority(); // от высокого к низкому
+        });
+}
+
+void TaskManager::saveToFile(const std::string& filename) const {
+    std::ofstream ofs(filename);
+    for (const auto& task : tasks) {
+        ofs << task->serialize() << "\n";
+    }
+}
+
+void TaskManager::loadFromFile(const std::string& filename) {
+    std::ifstream ifs(filename);
+    std::string line;
+    while (std::getline(ifs, line)) {
+        addTask(std::make_unique<Task>(Task::deserialize(line)));
+    }
+}
+
--- core/taskmanager.h
+++ core/taskmanager.h
@@ -1,2 +1,20 @@
 #pragma once
-// Заголовочный файл
+#include <vector>
+#include <memory>
+#include <string>
+#include "task.h" 
+
+class TaskManager {
+private:
+    std::vector<std::unique_ptr<Task>> tasks;
+
+public:
+    void addTask(std::unique_ptr<Task> task);
+    void removeTask(std::size_t index);
+    void listTasks(bool showCompleted = true) const;  //Выводит на экран список задач
+    void sortByPriority();
+
+    void saveToFile(const std::string& filename) const; //Сохраняет все задачи в файл
+    void loadFromFile(const std::string& filename); //Загружает задачи из файла.
+};
+
